c327722e9b4627a06a7769a2d820331ad56769ca
ThanThai21 <thaibk210@gmail.com>
2017-07-24 23:04:14 +0700

    commit

---
+++ app/src/main/java/com/esp/hangul/CustomView/CustomTextView.java
@@ -0,0 +1,40 @@
+package com.esp.hangul.CustomView;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class CustomTextView extends TextView {
+    public CustomTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CustomTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CustomTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "font.ttf");
+            setTypeface(tf);
+        }
+
+    }
+}
